7fb11ef6126d4572545b53ce1a08adf43ef5ea6b
ChenxiiCheng <chenxic1011@gmail.com>
2020-01-30 21:38:30 -0500

    first commit

---
+++ src/components/layout/Alert.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Alert = () => {
+  const { alert } = useContext(AlertContext);
+  return (
+    alert !== undefined && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle" /> {alert.msg}
+      </div>
+    )
+  );
+};
+
+export default Alert;
